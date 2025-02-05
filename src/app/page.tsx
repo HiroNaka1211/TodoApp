23c2db83ebb67b4df95b93713b302ac87510e48d
HiroNaka1211 <njim1211@gmail.com>
2025-02-05 22:44:49 +0900

    add api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,16 @@
+import { getAllTodos } from "@/api";
 import AddTodo from "./components/addTodo";
 import TodoList from "./components/todoList";
 import style from "./styles/style.module.css";
 
-export default function Home() {
+export default async function Home() {
+  const todos = await getAllTodos();
   return (
     <main className={style.container}>
       <h1 className={style.title}>Todo App</h1>
       <div className={style.inputArea}>
         <AddTodo />
-        <TodoList />
+        <TodoList todos={todos} />
       </div>
     </main>
   );
