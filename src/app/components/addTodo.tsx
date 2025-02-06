d4a329aed61f45b65938e49d27589a4ebbd7a17a
HiroNaka1211 <njim1211@gmail.com>
2025-02-06 22:52:10 +0900

    add add and delete function

--- src/app/components/addTodo.tsx
+++ src/app/components/addTodo.tsx
@@ -2,18 +2,27 @@
 import { useRouter } from "next/navigation";
 import { ChangeEvent, FormEvent, useState } from "react";
 import style from "../styles/addTodo.module.css";
+import { addTodo } from "@/api";
+import { TodoType } from "@/type";
+import { v4 as uuidv4 } from "uuid";
 
 export default function AddTodo() {
   const router = useRouter();
   const [input, setInput] = useState<string>("");
   const [errorMessage, seterrorMessage] = useState(false);
 
-  const handleSubmit = (e: FormEvent) => {
+  const handleSubmit = async (e: FormEvent) => {
     e.preventDefault();
     if (input === "") {
       seterrorMessage(true);
       return;
     }
+    const todo: TodoType = {
+      id: uuidv4(),
+      text: input,
+      completed: false,
+    };
+    await addTodo(todo);
     setInput("");
     window.alert(input + "を登録しました。");
     router.refresh();
